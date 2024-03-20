30dbec837f9032cf637c3cc622973d22463283c2
Franz Geiselbrechtinger <fg@primeinteractive.de>
2024-03-21 00:40:17 +0100

    feat: add path handling

--- src/app.js
+++ src/app.js
@@ -10,14 +10,15 @@ const logger = require("./logger");
 
 logger.log({
   level: "info",
-  message: "Application start",
+  message: "⚡️ Application is starting",
 });
 
 app.use(express.static("static"));
 app.use(express.json());
 
-const guard = (path, res) => {
-  if (!path || path.includes(".")) {
+const guard = (last, path, res) => {
+  // TODO: create test case for path and last with dots
+  if (!last || last.includes(".") || (path && path.includes("."))) {
     res.status(404).send({ status: "notok", error: "File not found" });
     return true;
   }
@@ -26,13 +27,15 @@ const guard = (path, res) => {
 
 app.get("/structure/:path*", (req, res) => {
   res.header("Content-Type", "application/json");
-  const pathParam = req.params?.path;
+  const last = req.params?.path;
+  // TODO: Add path verfication
+  const path = req.params[0];
 
-  if (guard(pathParam, res)) return;
+  if (guard(last, path, res)) return;
 
   //TODO: improve error handling
 
-  const data = file.read(`structure/${pathParam}`);
+  const data = file.read("structure", path, last);
 
   if (data) {
     res.status(200).send(data);
@@ -43,15 +46,19 @@ app.get("/structure/:path*", (req, res) => {
 
 app.post("/structure/:path*", (req, res) => {
   res.header("Content-Type", "application/json");
-  const pathParam = req.params?.path;
+
+  const ressource = req.params?.path;
+  const path = req.params[0];
+
   const content = JSON.stringify(req.body);
 
-  if (guard(pathParam, res)) return;
+  if (guard(ressource, path, res)) return;
+
   //TODO: verify json content
 
-  const data = file.read(`structure/${pathParam}`);
+  // const data = file.read(`structure/${pathParam}`, "structure");
 
-  if (file.write(`structure/${pathParam}`)) {
+  if (file.write("structure", path, ressource, content)) {
     res.status(201).send({ status: "ok", content: JSON.parse(content) });
   } else {
     res.status(404).send({ status: "notok", error: "File not found" });
--- src/file.js
+++ src/file.js
@@ -1,25 +1,36 @@
-const fs = require("fs").promises;
+const fsp = require("fs").promises;
+const { existsSync } = require("fs");
 
 const logger = require("./logger");
 
 const DATA_PATH = "data";
 
 const fileModule = {
-  read: async (path) => {
+  read: async (section, path, ressource) => {
     try {
-      return await fs.readFile(`${DATA_PATH}/${path}.json`);
+      return await fsp.readFile(
+        `${DATA_PATH}/${section}${path}/${ressource}.json`,
+      );
     } catch (err) {
       logger.error(`Error reading file ${path}`, err);
       return false;
     }
   },
 
-  write: async (path, content) => {
+  write: async (section, path, ressource, content) => {
+    const targetPath = `./${DATA_PATH}/${section}${path}`;
     try {
-      await fs.writeFile(`${DATA_PATH}/${path}.json`, content);
+      if (!existsSync(targetPath)) {
+        const dir = await fsp.mkdir(targetPath, { recursive: true });
+        if (!dir)
+          logger.error({
+            message: "Directory was not created",
+          });
+      }
+      await fsp.writeFile(`${targetPath}/${ressource}.json`, content);
       return true;
     } catch (err) {
-      logger.error(`Error writing file ${path}`, err);
+      logger.error(`Error writing file ${targetPath}}`, err);
       return false;
     }
   },
